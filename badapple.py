07d23c86e9b354279229f3e5d1fa9255f5f1b24e
CDavidSV <88672259+CDavidSV@users.noreply.github.com>
2022-12-28 13:29:49 -0800

    A few improvements
    - Renders frames before actually displaying the video.
    - Little optimizations.

--- badapple.py
+++ badapple.py
@@ -8,11 +8,13 @@ import cv2
 from PIL import Image
 import time
 
-ASCIICharacters = [' ', ',', ':', ';', '+', '*', '?', '%', 'S', '#', '@']
+ASCIICharacters = [' ', '.', ',', ':', ';', '!', '+', '*', '?', '%', 'S', '#', '@']
 desiredWidth = 150
+asciiFrames = []
 
 # Path to the frames folder.
-fileName = 'bad_apple1.mp4'
+videoFile = 'bad_apple.mp4' # mp4
+audioFile = 'bad_apple.mp3' # mp3
 
 # Resizes the image to fit in the terminal (with specified width).
 def resizeImage(image, width, height):
@@ -23,30 +25,55 @@ def resizeImage(image, width, height):
 def generateFrame(image, width, height):
     resizedImage = resizeImage(image, width, height)
     pixels = resizedImage.getdata()
-    characters = "".join([ASCIICharacters[pixel//25] for pixel in pixels])
+    characters = "".join([ASCIICharacters[pixel//20] for pixel in pixels])
     totalPixels = len(characters)
     ASCIIimage = "\n".join([characters[index:(index + width)] for index in range(0, totalPixels, width)])
-    sys.stdout.write(ASCIIimage)
+    asciiFrames.append(ASCIIimage)
 
+def progressBar(progressDone, totalProgress):
+    progressDonePercentage = progressDone / totalProgress * 100
+
+    total = 80
+    progreessString = '|'
+
+    for i in range(0, total):
+        if i <= progressDonePercentage * total / 100:
+            progreessString += '█'
+        else:
+           progreessString += ' '
+    progreessString += f'| {round(progressDonePercentage, 1)}%'
+
+    return progreessString
 
 # Main loop that goes through all frames from the video.
 if __name__ == '__main__':
-    video = cv2.VideoCapture(os.path.abspath(f'./{fileName}'))
+    video = cv2.VideoCapture(os.path.abspath(f'./{videoFile}'))
     fps = video.get(cv2.CAP_PROP_FPS)
+    totalFrames = video.get(cv2.CAP_PROP_FRAME_COUNT)
     oldWidth = video.get(cv2.CAP_PROP_FRAME_WIDTH)
     oldHeight = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
     AR = oldWidth / oldHeight
     newHeight = (desiredWidth / AR) / 2
     sleepTime = 1 / fps
-
-    playsound("bad_apple.mp3", False)
+    
+    renderedFrames = 0
     while (True):
         # Read video frame.
         ret, frame = video.read()
-
+        
         if ret:
-            os.system('cls')
             generateFrame(Image.fromarray(frame), desiredWidth, newHeight)
-            time.sleep(sleepTime - (time.time() % sleepTime))
+            renderedFrames += 1
+            os.system('cls')
+            print("Rendering frames...")
+            print(progressBar(renderedFrames, totalFrames))
         else:
             break
+
+    playsound(audioFile, False)
+    for frame in asciiFrames:
+        os.system('cls')
+        sys.stdout.write(frame)
+        time.sleep(sleepTime - (time.time() % sleepTime)) # Make sure the time between each frame is the same.
+    os.system('cls')
